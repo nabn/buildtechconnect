8ba228f4b825ba4b702fc708d6379ff98c3cd2d5
nabn <nabeen.khadka@gmail.com>
2024-08-05 11:38:42 +1000

    Add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config"
-
 import tailwind from "@astrojs/tailwind"
+import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://www.buildtechconnect.com",
+  integrations: [tailwind(), robotsTxt()],
 })
+
