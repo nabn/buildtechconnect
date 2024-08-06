93be39a04cde1551f7cd2eaa1b31a5fb8f623fa4
nabn <nabeen.khadka@gmail.com>
2024-08-06 18:56:21 +1000

    Add Cloudflare SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import robotsTxt from "astro-robots-txt"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.buildtechconnect.com",
   integrations: [tailwind(), robotsTxt()],
-})
-
+  output: "server",
+  adapter: cloudflare()
+});
