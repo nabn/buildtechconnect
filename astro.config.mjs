d3eaa299a67942919973d8d577bd48c442a27e6d
nabn <nabeen.khadka@gmail.com>
2024-08-06 19:08:48 +1000

    Configure Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import robotsTxt from "astro-robots-txt"
 
+import netlify from "@astrojs/netlify"
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.buildtechconnect.com",
   integrations: [tailwind(), robotsTxt()],
+  output: "server",
+  adapter: netlify(),
 })
